c3d81921826e23474cc176212188cc72b755e387
shchepachkov.d.v <shchepachkov.d.v@sbertech.ru>
2025-03-16 18:20:07 +0300

    inital commit

---
+++ src/main/java/ru/schepachkov/springlifecycle/SpringlifecycleApplication.java
@@ -0,0 +1,13 @@
+package ru.schepachkov.springlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlifecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlifecycleApplication.class, args);
+	}
+
+}
